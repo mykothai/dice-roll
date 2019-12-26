a72d7b062c0ba132832455b3016b1ee968803e3c
mykothai <mike.thai2303@gmail.com>
2019-12-25 22:11:40 -0800

    Remove comments and improve formatting

--- dice_roll.c
+++ dice_roll.c
@@ -33,8 +33,8 @@ struct Options {
 };
 
 // function that CAPITALIZES user input
-char* strupr(char* s)
-{
+// https://stackoverflow.com/questions/26327812/strupr-and-strlwr-in-string-h-part-are-of-the-ansi-standard/26328095#26328095
+char* strupr(char* s) {
     char* tmp = s;
 
     for (;*tmp;++tmp) {
@@ -44,7 +44,7 @@ char* strupr(char* s)
 }
 
 // Initializes program by printing header
-void init(){
+void init() {
     srand(time(NULL));
 
     printf("\n");
@@ -55,15 +55,15 @@ void init(){
     printf(" ==============================================================================\n");
 }
 
-void printPlayers(struct ThePlayers* players){
+void printPlayers(struct ThePlayers* players) {
     int i = 0;
-    for(i = 0; i < players->numberPlayers; i++){
+    for (i = 0; i < players->numberPlayers; i++) {
         printf("Player %s's Turn!\n", players->playerNames[i]);
     }
 }
 
 // Reads N names and stores them in an array
-void addPlayers(struct ThePlayers* players){
+void addPlayers(struct ThePlayers* players) {
     int i = 0;
     char yesNo;
     players->numberPlayers = 0;
@@ -71,10 +71,10 @@ void addPlayers(struct ThePlayers* players){
     printf(" Do you want to add players to this session? [y/n] ");
     scanf("%c", &yesNo);
 
-    if(yesNo == 'y' || yesNo == 'Y'){
+    if (yesNo == 'y' || yesNo == 'Y') {
         printf(" How many players? ");
 
-        while(scanf("%d", &players->numberPlayers) != 1){   // checks if number of players user wants is an integer
+        while (scanf("%d", &players->numberPlayers) != 1) {   // checks if number of players user wants is an integer
             getchar();                                      // discard user input since its not an integer and prompt again
             printf(" That's not a number...think about your life choices and try again.\n");
         }
@@ -82,7 +82,7 @@ void addPlayers(struct ThePlayers* players){
         // players->playerNames[players->numberPlayers];
         printf(" Enter %d player names in the order of play.\n", players->numberPlayers);
         players->playerNames[players->numberPlayers][100];
-        for(i = 0; i < players->numberPlayers; i++){
+        for (i = 0; i < players->numberPlayers; i++) {
             scanf("%s", players->playerNames[i]);
         }
         printf("\n");
@@ -91,33 +91,33 @@ void addPlayers(struct ThePlayers* players){
         printf("  =============================================\n");
         printf("  Player Order:                                \n");
         printf("  ---------------------------------------------\n");
-        for (i = 0; i < players->numberPlayers; i++){
+        for (i = 0; i < players->numberPlayers; i++) {
             printf("  Player %d: %s                            \n", i+1, strupr(players->playerNames[i]));
         }
         printf("  =============================================\n");
     }
 
-    else if(yesNo == 'n' || yesNo =='N'){
+    else if (yesNo == 'n' || yesNo =='N') {
         //do nothing
     }
 
-    else{
+    else {
         printf(" User input error...bye.\n");
         exit(-1);
     }
 }
 
-void enableHistogram(struct Options* optionSettings){
+void enableHistogram(struct Options* optionSettings) {
     char yesNoHistogram;
     optionSettings->histogramOn = 0;
 
     printf("\n Show histogram after each roll? [y/n] ");
-    while(1){
+    while(1) {
         scanf("%c", &yesNoHistogram);
-        if(yesNoHistogram == 'n' || yesNoHistogram == 'N'){
+        if (yesNoHistogram == 'n' || yesNoHistogram == 'N') {
             break;
         }
-        else if(yesNoHistogram == 'y' || yesNoHistogram == 'Y'){
+        else if (yesNoHistogram == 'y' || yesNoHistogram == 'Y') {
             optionSettings->histogramOn = 1;
             break;
         }
@@ -125,14 +125,19 @@ void enableHistogram(struct Options* optionSettings){
     printf("\n");
 }
 
-void numberOfDiceToRoll(struct Histogram* hist, struct ThePlayers* players, struct Options* optionSettings){
+void numberOfDiceToRoll(struct Histogram* hist, struct ThePlayers* players, struct Options* optionSettings) {
     int i = 0, j = 0, k = 0, m = 0;
-    int sum = 0, rollResult = 0, sumCounter = 0, playerCounter = 0, whoseTurn = 0;
-    int numberOfDiceToRoll = 0, keypress = 0;
+    int sum = 0, 
+        rollResult = 0,
+        sumCounter = 0, 
+        playerCounter = 0, 
+        whoseTurn = 0,
+        numberOfDiceToRoll = 0, 
+        keypress = 0;
 
     printf(" How many dice do you want to roll? ");
     
-    while(scanf("%d", &numberOfDiceToRoll) != 1){       // checks if number of dice user wants to roll is an integer
+    while (scanf("%d", &numberOfDiceToRoll) != 1) {       // checks if number of dice user wants to roll is an integer
         getchar();                                      // discard user input since its not an integer and prompt again
         printf(" That's not a number...try again.\n");
     }
@@ -141,7 +146,7 @@ void numberOfDiceToRoll(struct Histogram* hist, struct ThePlayers* players, stru
     hist->arraySize = DICE_SIDES*numberOfDiceToRoll;
     hist->histogramArray[hist->arraySize];
 
-    for (i = 0; i < hist->arraySize; i++){
+    for (i = 0; i < hist->arraySize; i++) {
         hist->histogramArray[i] = 0;                    // set all elements in new histogram to zero
     }
 
@@ -149,11 +154,11 @@ void numberOfDiceToRoll(struct Histogram* hist, struct ThePlayers* players, stru
     printf(" Press ENTER to roll %d dice.\n", numberOfDiceToRoll);
     char input = scanf("%lc", &keypress);
 
-    while(input != EOF){
-        if(keypress == '\n' || keypress == ' '){ // can also use getchar()
+    while (input != EOF) {
+        if (keypress == '\n' || keypress == ' ') { // can also use getchar()
             printf(" ---------------------------------------------\n");
 
-            for(j = 0; j < numberOfDiceToRoll; j++){
+            for (j = 0; j < numberOfDiceToRoll; j++) {
                 rollResult = rand()%DICE_SIDES+1;
                 printf("   Dice %d:\n", j+1);
 
@@ -165,7 +170,7 @@ void numberOfDiceToRoll(struct Histogram* hist, struct ThePlayers* players, stru
             }
             printf(" #############################################\n");
 
-            if(players->numberPlayers != 0){
+            if (players->numberPlayers != 0) {
                 whoseTurn = playerCounter % players->numberPlayers;
                 printf(" ROUND %d                      Player: %s\n", (int)floor(playerCounter/players->numberPlayers)+1, strupr(players->playerNames[whoseTurn]));
 /*
@@ -190,22 +195,20 @@ void numberOfDiceToRoll(struct Histogram* hist, struct ThePlayers* players, stru
             printf("\n");
 
             // print histogram after each roll if histogramOn is enabled
-            if(optionSettings->histogramOn == 1){
-                for (k = 0; k < hist->arraySize; k++){
+            if (optionSettings->histogramOn == 1) {
+                for (k = 0; k < hist->arraySize; k++) {
                     printf("        [%2d]: \t", k+1);
 
-                    for (m = 0; m < hist->histogramArray[k]; m++){
+                    for (m = 0; m < hist->histogramArray[k]; m++) {
                         printf("#");
                     }
                     printf("\n");
-
-
                 }
             }
         }
 
         // exit program if user types "exit" and hits enter
-        else if(keypress == 'Q'){    // '033' is ascii for esc, try k = 27
+        else if (keypress == 'Q') {    // '033' is ascii for esc, try k = 27
             break;
         }
 
@@ -216,7 +219,7 @@ void numberOfDiceToRoll(struct Histogram* hist, struct ThePlayers* players, stru
     }
 }
 
-void printHistogram(struct Histogram* hist){
+void printHistogram(struct Histogram* hist) {
     int i = 0, total = 0, longestResult = 100;
     double percentage = 0;
 
@@ -267,16 +270,16 @@ void printHistogram(struct Histogram* hist){
     printf("\n =============================================\n");
 }
 
-int exitProg(int exitCondition){
+int exitProg(int exitCondition) {
     char lastInput, lastPress;
-    while(1){
+    while(1) {
         printf(" Press 'R' to restart or 'Q' to exit program\n");
         scanf("%c", &lastPress);
-        if(lastPress == 'R'){
+        if (lastPress == 'R') {
             getchar();
             break;
         }
-        else if(lastPress == 'Q'){
+        else if (lastPress == 'Q') {
             exitCondition = false;
             getchar();
             return 0;
@@ -285,14 +288,14 @@ int exitProg(int exitCondition){
     return 1;
 }
 
-int main(){
+int main() {
     struct ThePlayers   players;
     struct Options      optionSettings;
     struct Histogram    hist;
 
     int exitCondition = true;                   // enables program to run indefinately until func call exitProg and user quits
 
-    while(exitCondition){
+    while(exitCondition) {
         // Initialize srand and start program
         init();
 
@@ -309,21 +312,11 @@ int main(){
         printHistogram(&hist);
    
         getchar();                              // throw away last input so it doesn't affect the next input request
-        if (exitProg(exitCondition) == 0){      // exitProg returns 0 means exitCondition changed to false so program exits successfully
+
+        if (exitProg(exitCondition) == 0) {      // exitProg returns 0 means exitCondition changed to false so program exits successfully
             break;
         }
     }
     return 0;
 }
 
-/* // for single roll of 2 die
-
-for (i = 0; i < NUMBER_DICE; i++) {
-    diceRoll = rand()%6+1;
-    printf("%d\n", diceRoll);
-    sum += diceRoll;
-}
-freqArr[sum]++;
-printf("Sum of dice roll is: %d\n\n", sum);
-
-*/
